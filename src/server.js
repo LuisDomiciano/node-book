3d647f066bbf08301d1bf76848c6c7792a584fe3
LuisD <luis.domiciano@hotmail.com>
2022-04-22 09:06:40 -0300

    refactor: move code application to app.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,5 @@
-import express from "express";
+import app from "./app";
 
-const app = express();
 const port = 3333;
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.get('/', (request, response) => response.send("Hello World!"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
