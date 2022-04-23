c0d3a0655bdcd64c253586815638b45c792ecd61
LuisD <luis.domiciano@hotmail.com>
2022-04-23 17:21:02 -0300

    chore: configure test unit

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,9 @@
+import ProductsController from "../../../src/controllers/products";
+
+describe("Controllers: Products", () => {
+  describe("get() products =", () => {
+    it("should return a list products", () => {
+      const productsController = new ProductsController();
+    });
+  });
+});
