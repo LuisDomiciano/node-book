23db5c4905c8bc272e296726fe7f2cf89daf7a9a
LuisD <luis.domiciano@hotmail.com>
2022-04-23 14:02:38 -0300

    feat: should return a list of products default

--- src/app.js
+++ src/app.js
@@ -7,4 +7,10 @@ app.use(express.urlencoded({extended: false}));
 
 app.get('/', (request, response) => response.send("Hello World!"));
 
+app.get('/products', (request, response) => response.send([{
+  name: "Default product",
+  description: "product description",
+  price: 100
+}]));
+
 export default app;
